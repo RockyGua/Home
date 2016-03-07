79b6837bb0bed5f89f8df857d53adf6f29215c34
RockyGua <330516835@qq.com>
2016-03-07 10:22:10 +0800

    get servlet api

--- struts2/struts2_login/src/main/java/com.rocky.original/LoginAction.java
+++ struts2/struts2_login/src/main/java/com.rocky.original/LoginAction.java
@@ -1,5 +1,11 @@
 package com.rocky.original;
 
+import com.opensymphony.xwork2.ActionContext;
+import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.StrutsStatics;
+
+import javax.servlet.http.HttpServletRequest;
+
 public class LoginAction {
     private String userName;
     private String pwd;
@@ -7,6 +13,11 @@ public class LoginAction {
     public String execute() {
         if (userName.equals("admin") && pwd.equals("123456"))
         {
+            HttpServletRequest request = (HttpServletRequest)ActionContext.getContext().get(StrutsStatics.HTTP_REQUEST);
+            request.getSession().setAttribute("user", userName);
+            //第二种获取request的方式：request = (HttpServletRequest)ServletActionContext.getRequest();
+            //第三种获取方式：通过implements ServletRequestAware
+
             return "success";
         }
         return "failed";
