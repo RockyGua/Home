4246ed98126166678adb88c69f31be1045c7630e
RockyGua <330516835@qq.com>
2016-04-26 16:48:09 +0800

    insert方法实现

--- jdbc/sorm/src/main/java/com/rocky/sorm/core/MysqlQuery.java
+++ jdbc/sorm/src/main/java/com/rocky/sorm/core/MysqlQuery.java
@@ -6,17 +6,24 @@ import com.rocky.sorm.bean.TableInfo;
 import com.rocky.sorm.utils.JDBCUtils;
 import com.rocky.sorm.utils.ReflectUtils;
 
+import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MysqlQuery implements Query {
 
     public static void main(String[] args){
         Emp e = new Emp();
-        e.setId(0);
-        new MysqlQuery().delete(e);
+//        e.setId(2);//已设置主键id为自增长，不需要再赋值
+        e.setEmpname("rocky");
+        e.setBirthday(new java.sql.Date(System.currentTimeMillis()));
+        e.setAge(30);
+        e.setSalary(3000.8);
+//		new MysqlQuery().delete(e);
+		new MysqlQuery().insert(e);
     }
 
     public int executeDML(String sql, Object[] params) {
@@ -36,7 +43,35 @@ public class MysqlQuery implements Query {
     }
 
     public void insert(Object obj) {
+        //obj-->表中。             insert into 表名  (id,uname,pwd) values (?,?,?)
+        Class c = obj.getClass();
+        //存储sql的参数对象
+        List<Object> params = new ArrayList<Object>();
+        TableInfo tableInfo = TableContext.poClassTableMap.get(c);
+        StringBuilder sql  = new StringBuilder("insert into "+tableInfo.getTname()+" (");
 
+        //计算不为null的属性值
+        int countNotNullField = 0;
+        Field[] fs = c.getDeclaredFields();
+        for(Field f:fs){
+            String fieldName = f.getName();
+            Object fieldValue = ReflectUtils.invokeGet(fieldName, obj);
+
+            if(fieldValue!=null){
+                countNotNullField++;
+                sql.append(fieldName+",");
+                params.add(fieldValue);
+            }
+        }
+
+        sql.setCharAt(sql.length()-1, ')');
+        sql.append(" values (");
+        for(int i=0;i<countNotNullField;i++){
+            sql.append("?,");
+        }
+        sql.setCharAt(sql.length()-1, ')');
+
+        executeDML(sql.toString(), params.toArray());
     }
 
     public void delete(Class clazz, Object id) {
