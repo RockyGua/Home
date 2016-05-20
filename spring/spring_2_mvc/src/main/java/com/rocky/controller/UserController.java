3f991d07dfbb78c98ee36d949a4714e29d51a569
RockyGua <330516835@qq.com>
2016-05-20 15:23:56 +0800

    spring mvc - show entity

---
+++ spring/spring_2_mvc/src/main/java/com/rocky/controller/UserController.java
@@ -0,0 +1,31 @@
+package com.rocky.controller;
+
+import com.rocky.model.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    @RequestMapping("/list")
+    public ModelAndView list(Model model) {
+        User user = new User();
+        user.setUserName("uName");
+        user.setPassword("uPwd");
+        model.addAttribute(user);
+        return new ModelAndView("/WEB-INF/views/user.jsp");
+    }
+
+//    http://localhost:8080/user/name?uName=name
+    @RequestMapping("/name")
+    public ModelAndView detail(String uName, Model model) {
+        User user = new User();
+        user.setUserName(uName);
+        user.setPassword("uPwd");
+        model.addAttribute(user);
+        return new ModelAndView("/WEB-INF/views/user.jsp");
+    }
+}
---
+++ spring/spring_2_mvc/src/main/webapp/WEB-INF/views/user.jsp
@@ -0,0 +1,12 @@
+
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<%@ page isELIgnored="false"%>
+<html>
+<head>
+    <title></title>
+  User:${user.userName},${user.password}
+</head>
+<body>
+
+</body>
+</html>
